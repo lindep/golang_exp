7528647fabfdad69eeee815d3766cb39c2b91fec
Pieter Linde <pieter.linde@gmail.com>
2017-01-16 05:23:08 +0000

    Add argv and file read

---
+++ testfile/test.go
@@ -0,0 +1,38 @@
+
+package main
+
+import (
+	_ "flag"
+	"fmt"
+	"os"
+	"log"
+)
+
+func main() {
+
+	args := os.Args
+	fmt.Printf("argv flags = %d\n", len(args));
+  if len(args) < 1 {
+      fmt.Println("Input file is missing.");
+      os.Exit(1);
+  }
+  fileName := os.Args[1]
+
+	file, err := os.Open(fileName) // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("data type = %T\n", data)
+	fmt.Printf("read %d bytes: %q\n", count, data[3:count])
+
+	
+	fmt.Println("Hello, 世界")
+}
+
+
