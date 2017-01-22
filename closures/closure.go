7203cd2f3789385014eea1d5190251f6ea48910c
testuser <testuser@nowhere.com>
2017-01-22 21:00:03 +1300

    more closure examples

--- closures/closure.go
+++ closures/closure.go
@@ -2,25 +2,37 @@ package main
 
 import "fmt"
 
-func firstFun() func() string {
+func firstFun(string) func() string {
   return func() string {
       return "second func"
     }
 }
+func getDir(dir string) func() {
+  return func() {
+      fmt.Println(dir,"valid variable scope")
+    }
+}
 
 func main() {
-  dirs := []string{"one","two"}
-  var rmdirs []func()
+  dirs := [2]string{"one","two"}
+  var rmdirsInvalid []func()
+  var rmdirsValid []func()
   for _, dir := range dirs {
-    fmt.Println(dir)
-    dir := dir
-    rmdirs = append(rmdirs, func() {
-      fmt.Println(dir)
+    fmt.Println("Add dir",dir)
+    //dir := dir
+    //f := getDir(dir)
+    rmdirsInvalid = append(rmdirsInvalid, func() {
+      fmt.Println(dir, "invalid variable scope")
     })
+    rmdirsValid = append(rmdirsValid, getDir(dir))
   }
 
-  for _, rmdir := range rmdirs {
-    rmdir()
+  for _, rmdirInvalid := range rmdirsInvalid {
+    rmdirInvalid()
+  }
+
+  for _, rmdirValid := range rmdirsValid {
+    rmdirValid()
   }
 
 }
