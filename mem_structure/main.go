9e5bff6b66c960f650cbd7025048caa2bca7f1dd
Pieter Linde <pieter.linde@gmail.com>
2017-02-08 03:33:04 +0000

    Mem layout changes

--- mem_structure/main.go
+++ mem_structure/main.go
@@ -1,6 +1,6 @@
 
 /*
-Using this Meduim artical "Go and memory layout"
+Using this Meduim article "Go and memory layout"
 https://hackernoon.com/go-and-memory-layout-6ef30c730d51#.c72d7kxns
 */
 
@@ -9,13 +9,14 @@ package main
 import (
 	"fmt"
   "reflect"
+	"unsafe"
 )
 
 type MyData struct {
   aByte   byte
   //anotherByte byte
-  aanInt32 int32
-  aString string
+  //aanInt32 int32
+  //aString string
   aShort  int16
   anInt32 int32
   aSlice  []byte
@@ -34,4 +35,17 @@ func main() {
         field.Name, field.Offset, field.Type.Size(),
         field.Type.Align())
    }
+
+	 data := MyData{
+		aByte: 0x1,
+		//aString: "a",
+		aShort: 0x0203,
+		anInt32: 0x04050607,
+		aSlice: []byte{
+			0x08, 0x09, 0x0a,
+		},
+	}
+
+	 dataBytes := (*[32]byte)(unsafe.Pointer(&data))
+	 fmt.Printf("Bytes are \n%#v\n", dataBytes)
 }
