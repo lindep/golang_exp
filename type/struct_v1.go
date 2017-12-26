5bcce100681fe88c15846dc21f6e27b0309350e7
Pieter Linde <pieter.linde@gmail.com>
2017-12-26 13:56:41 +1300

    add struct example

---
+++ type/struct_v1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+}
+
+func main() {
+	p1 := person{
+		First: "a",
+		Last:  "z",
+	}
+	p2 := person{
+		First: "b",
+		Last:  "y",
+	}
+	xp := []person{p1, p2}
+
+	fmt.Println("go data", xp)
+
+	bs, err := json.Marshal(xp)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("json", string(bs))
+	}
+
+	j := `[{"First":"a","Last":"z"},{"First":"b","Last":"y"}]`
+	fmt.Println("json", j)
+	xp1 := []person{}
+	errs := json.Unmarshal([]byte(j), &xp1)
+	if errs != nil {
+		fmt.Println(errs)
+	} else {
+    fmt.Printf("\n%+v\n", xp1) // + also print the field names of the struct
+  }
+}
