8c4e799a746aff0ee82bdd3baba31010d8a577b0
Pieter Linde <pieter.linde@gmail.com>
2018-03-17 14:10:16 +1300

    Example init funtions

---
+++ init_functions/sandbox.go
@@ -0,0 +1,15 @@
+
+package main
+
+import "fmt"
+
+var (
+    a = c - 2
+    b = 2
+)
+
+func main() {
+    fmt.Println(a)
+    fmt.Println(b)
+    fmt.Println(c)
+}
---
+++ init_functions/utils.go
@@ -0,0 +1,8 @@
+
+package main
+
+var c = f()
+
+func f() int {
+    return b + 1
+}
